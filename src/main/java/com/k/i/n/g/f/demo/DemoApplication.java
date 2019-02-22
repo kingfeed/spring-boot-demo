8898a53c25bf36357f14d02d50e780fa64e4ff03
dongfei <df@choicesoft.com.cn>
2019-02-22 17:44:24 +0800

    logback

--- src/main/java/com/k/i/n/g/f/demo/DemoApplication.java
+++ src/main/java/com/k/i/n/g/f/demo/DemoApplication.java
@@ -8,12 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@MapperScan({"com.k.i.n.g.f.demo.dao"})
-
 /**
  * @author dongfei
  */
+
+@SpringBootApplication
+@MapperScan({"com.k.i.n.g.f.demo.dao"})
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
