8d9bcc4cfc76232ba553642cd2728a04629d0baf
dongfei <df@choicesoft.com.cn>
2019-07-22 16:57:47 +0800

    测试redis-son 锁

--- src/main/java/com/k/i/n/g/f/demo/controller/DemoRedisSonController.java
+++ src/main/java/com/k/i/n/g/f/demo/controller/DemoRedisSonController.java
@@ -1,5 +1,6 @@
 package com.k.i.n.g.f.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -16,8 +17,9 @@ import java.util.concurrent.TimeUnit;
  * @author dongfei
  */
 @RestController
-@RequestMapping("redisson")
-public class DemoRedissonController {
+@RequestMapping("redis-son")
+@Slf4j
+public class DemoRedisSonController {
     @Autowired
     private RedissonClient redissonClient;
     @Autowired
@@ -25,14 +27,21 @@ public class DemoRedissonController {
 
     @RequestMapping("/lock/{id}")
     public Integer doLock(@PathVariable(name = "id") Integer id){
-        redisTemplate.opsForValue().increment("id");
+        redisTemplate.opsForValue().increment(String.valueOf(id));
         RLock lock = redissonClient.getLock("pet/" + id);
         try {
-            lock.lock(30, TimeUnit.SECONDS);
+            // 尝试获取锁，等待3秒，持有时间30秒
+            boolean locked = lock.tryLock(3L,30L,TimeUnit.SECONDS);
+            //  lock.lock(30L, TimeUnit.SECONDS) 加锁不设置等待时间，一直等待直到获取或者线程被打断;
+            if(!locked){
+                log.error("locked on {}",id);
+            }
+            log.info("lock id {}",id);
         }catch (Exception ex){
-
+            log.error("lock exception",ex);
         }finally {
            lock.unlock();
+            redisTemplate.opsForValue().increment(String.valueOf(id),-1);
         }
         return 0;
     }
