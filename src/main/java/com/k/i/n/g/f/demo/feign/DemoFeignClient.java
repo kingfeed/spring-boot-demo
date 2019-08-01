e1bf1e824a48e1028620a9ab1b9d071f4790efdb
dongfei <df@choicesoft.com.cn>
2019-08-01 13:01:38 +0800

    配置，Feign调用测试

--- src/main/java/com/k/i/n/g/f/demo/feign/DemoFeignClient.java
+++ src/main/java/com/k/i/n/g/f/demo/feign/DemoFeignClient.java
@@ -13,6 +13,11 @@ public interface DemoFeignClient {
      * 测试Feign调用
      * @return
      */
-    @GetMapping("demo/api")
+
+    @GetMapping("api")
     String printDate();
+    /**
+     * 由原来的demo/api 修改为api
+     * 原因：FeignDemo.class 增加上下文配置不再取配置文件的了 --server.servlet.context-path=/
+     */
 }
